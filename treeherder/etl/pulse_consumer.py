86d6c2d89245cd83e1c190a70f0265d821315a3d
Cameron Dawson <cdawson@mozilla.com>
2015-09-01 13:20:54 -0700

    mapping done for pulse messages

--- treeherder/etl/pulse_consumer.py
+++ treeherder/etl/pulse_consumer.py
@@ -2,6 +2,8 @@ import logging
 import os
 import yaml
 import jsonschema
+import time
+import datetime
 from collections import defaultdict
 
 from treeherder.model.derived.jobs import JobsModel
@@ -24,15 +26,29 @@ class PulseConsumer:
     # status can only transition from lower to higher order.  If a job comes
     # in to update a status to a lower order, it will be skipped as out of
     # sequence.
-    status_order = {
+    status_rank = {
         "unscheduled": 0,
         "pending": 1,
         "running": 2,
         "success": 3,
-        "fail": 4,
-        "exception": 5,
-        "canceled": 6
+        "fail": 3,
+        "exception": 3,
+        "canceled": 3
     }
+    completed_status_rank = 3
+    test_result_map = {
+        "success": "success",
+        "fail": "testfail",
+        "exception": "exception",
+        "canceled": "usercancel"
+    }
+    build_result_map = {
+        "success": "success",
+        "fail": "busted",
+        "exception": "exception",
+        "canceled": "usercancel"
+    }
+
 
     @staticmethod
     def get_jobs_schema():
@@ -51,47 +67,36 @@ class PulseConsumer:
                 rs_lookup = job_model.get_revision_resultset_lookup(
                     [x['origin']['revision'] for x in job_list])
                 storeable_job_list = []
-                for job in job_list:
-                    storeable_job_list.append({
-                        "revision_hash": rs_lookup[job['origin']['revision']],
-                        "job": {
-                            "job_guid": job['jobGuid'],
-                            "name": job['display'].get('jobName', ''),
-                            "job_symbol": job['display'].get('jobSymbol', ''),
-                            "group_name": job['display'].get('groupName', ''),
-                            "group_symbol": job['display'].get('groupSymbol', ''),
-
-
-                            "product_name": job.get("productName"),
-                            "state": self.getState(job),
-                            "result": self.getResult(job),
-                            "reason": "scheduler",
-                            "who": "sendchange-unittest",
-                            "submit_timestamp": 1365732271,
-                            "start_timestamp": "20130411165317",
-                            "end_timestamp": "1365733932",
-                            "machine": "tst-linux64-ec2-314",
-                            "build_url": "http://....",
-                            "build_platform": {
-                                "platform": "Ubuntu VM 12.04",
-                                "os_name": "linux",
-                                "architecture": "x86_64",
-                                "vm": "true"
-                            },
-                            "machine_platform": {
-                                "platform": "Ubuntu VM 12.04",
-                                "os_name": "linux",
-                                "architecture": "x86_64",
-                                "vm": true
-                            },
-                            "option_collection": {
-                                "opt": true
-                            },
-                            "log_references": job['log_references'],
-                            "artifacts": job['artifacts'],
-                        },
-                        "coalesced": []
-                    })
+                for pulse_job in job_list:
+                    if (pulse_job['status'] != "unscheduled"):
+                        try:
+                            storeable_job_list.append({
+                                "revision_hash": rs_lookup[pulse_job['origin']['revision']],
+                                "job": {
+                                    "job_guid": pulse_job['jobGuid'],
+                                    "name": pulse_job['display'].get('jobName', ''),
+                                    "job_symbol": pulse_job['display'].get('jobSymbol', ''),
+                                    "group_name": pulse_job['display'].get('groupName', ''),
+                                    "group_symbol": pulse_job['display'].get('groupSymbol', ''),
+                                    "product_name": pulse_job.get("productName"),
+                                    "state": self.get_state(pulse_job),
+                                    "result": self.get_result(pulse_job),
+                                    "reason": pulse_job["reason"],
+                                    "who": pulse_job["who"],
+                                    "submit_timestamp": self.to_timestamp(pulse_job['times']['scheduled']),
+                                    "start_timestamp": self.to_timestamp(pulse_job['times']['started']),
+                                    "end_timestamp": self.to_timestamp(pulse_job['times']['completed']),
+                                    "machine": self.get_machine(pulse_job),
+                                    "build_platform": self.get_platform(pulse_job['machine'].get("build", None)),
+                                    "machine_platform": self.get_platform(pulse_job['machine'].get("run", None)),
+                                    "option_collection": self.get_option_collection(pulse_job),
+                                    "log_references": pulse_job['log_references'],
+                                    "artifacts": pulse_job['artifacts'],
+                                },
+                                "coalesced": []
+                            })
+                        except AttributeError as e:
+                            logger.warn("Skipping job due to bad attribute", exc_info=1)
 
 
     def transform(self, pulse_job):
@@ -102,11 +107,51 @@ class PulseConsumer:
         """
         job_placeholders = []
 
+    def get_option_collection(self, job):
+        option_collection = {["opt", True]}
+        if "optionCollection" in job:
+            option_collection = {}
+            for option in job["optionCollection"]:
+                option_collection[option] = True
+        return option_collection
 
+    def get_platform(self, platform_src):
+        platform = None
+        if platform_src:
+            platform = {
+                "platform": platform_src['platform'],
+                "os_name": platform_src['osName'],
+                "architecture": platform_src['architecture']
+            }
+        return platform
 
+    def get_machine(self, job):
+        machine = "unknown"
+        if "build" in job["machine"]:
+            machine = job['machine']['build']['machineName']
+        if "run" in job["machine"]:
+            machine = job['machine']['run']['machineName']
+        return machine
 
+    def get_state(self, job):
+        status = job["status"]
+        state = "completed"
+        if status in ["pending", "running"]:
+            state = status
+        elif status == "unscheduled":
+            raise AttributeError("unscheduled not a supported status at this time.")
+        return state
 
+    def get_result(self, job):
+        result = "unknown"
+        status = job['status']
+        if self.status_order[status] >= self.completed_status_rank:
+            if job['jobKind'] == "build":
+                result = self.build_result_map[status]
+            else:
+                result = self.test_result_map[status]
 
+        return result
 
     def _get_validated_jobs_by_project(self, jobs_list):
         validated_jobs = defaultdict(list)
@@ -134,3 +179,8 @@ class PulseConsumer:
     def _load_artifacts(self, artifact_list):
         pass
 
+    def to_timestamp(datestr):
+        """get a timestamp from a datestr like 2014-03-31"""
+        return time.mktime(datetime.datetime.strptime(
+            datestr
+        ).timetuple())
